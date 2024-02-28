9906efef4925c6b9593c91cfcea7400f1d6c77ec
Simon Stucki <simon_stucki@hotmail.com>
2024-02-28 19:52:33 +0100

    Extract main compute shader

---
+++ src/tracer-shader.ts
@@ -0,0 +1,23 @@
+import tracerShader from "./tracer-shader.wgsl?raw";
+
+type Params = {
+  imageWidth: number;
+  imageHeight: number;
+  maxWorkgroupDimension: number;
+};
+
+const PARAM_PLACEHOLDER_MAP: Record<keyof Params, string> = {
+  imageWidth: "imageWidth",
+  imageHeight: "imageHeight",
+  maxWorkgroupDimension: "maxWorkgroupDimension",
+};
+
+export default function build(params: Params) {
+  const placeholders = Object.entries(PARAM_PLACEHOLDER_MAP) as [
+    keyof Params,
+    string
+  ][];
+  return placeholders.reduce((aggregate, [key, value]) => {
+    return aggregate.replaceAll(`\${${value}}`, `${params[key]}`);
+  }, tracerShader);
+}
