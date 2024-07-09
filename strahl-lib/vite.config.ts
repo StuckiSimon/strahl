19854f6f2ec5b37dfaeb8e0a45e1fdc0d80fa43a
Simon Stucki <simon_stucki@hotmail.com>
2024-07-09 18:30:42 +0200

    Setup basic lib build

--- strahl-lib/vite.config.ts
+++ strahl-lib/vite.config.ts
@@ -2,4 +2,10 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
   plugins: [basicSsl()],
+  build: {
+    lib: {
+      entry: "src/main.ts",
+      name: "strahl",
+    },
+  },
 };
