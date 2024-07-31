b004ccc10d926ec87e790efe81925adc88945ec2
Simon Stucki <simon_stucki@hotmail.com>
2024-07-31 19:28:29 +0200

    Integrate renderer into benchmark

--- strahl-lib/src/benchmark.ts
+++ strahl-lib/src/benchmark.ts
@@ -1,48 +1,154 @@
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { logGroup } from "./cpu-performance-logger";
+import { OpenPBRMaterial } from "./openpbr-material";
+import runPathTracer from "./path-tracer";
 
 const MODEL_URL_FULL = "models/series-61-rotated/61-serie-edit.gltf"; // 1'068'735
 const MODEL_URL_BARE_BONES = "models/series-61-simplified-2/61-serie-edit.gltf"; // 10'687
 const MODEL_URL_MID = "models/series-61-simplified/61-serie-edit.gltf"; // 106'873
 
+const gltfLoader = new GLTFLoader();
+
+async function loadGltf(url: string) {
+  return new Promise((resolve, reject) => {
+    gltfLoader.load(url, resolve, undefined, reject);
+  });
+}
+
+const defaultBlueMaterial = new OpenPBRMaterial();
+defaultBlueMaterial.oBaseColor = [0.0, 0.9, 1.0];
+
+const plasticYellow = new OpenPBRMaterial();
+plasticYellow.oBaseColor = [0.9, 0.7, 0.23];
+plasticYellow.oSpecularWeight = 0.01;
+
+const greenPlastic = new OpenPBRMaterial();
+greenPlastic.oBaseColor = [0.4, 0.6, 0.33];
+
+const redPlasticMaterial = new OpenPBRMaterial();
+redPlasticMaterial.oBaseColor = [0.67, 0.18, 0.12];
+redPlasticMaterial.oSpecularColor = [1.0, 1.0, 0.9];
+redPlasticMaterial.oSpecularWeight = 0.2;
+
+const metalTestMaterial = new OpenPBRMaterial();
+metalTestMaterial.oBaseColor = [0.9, 0.9, 0.9];
+metalTestMaterial.oBaseMetalness = 1.0;
+
+const metalMaterial = new OpenPBRMaterial();
+metalMaterial.oBaseColor = [0.9, 0.9, 0.9];
+metalMaterial.oBaseMetalness = 1.0;
+metalMaterial.oBaseDiffuseRoughness = 0.5;
+
+const roughMetalMaterial = new OpenPBRMaterial();
+roughMetalMaterial.oBaseColor = [0.9, 0.9, 0.9];
+
+const roughMetalMaterial2 = new OpenPBRMaterial();
+roughMetalMaterial2.oBaseColor = [0.9, 0.9, 0.9];
+roughMetalMaterial2.oSpecularWeight = 0.0;
+roughMetalMaterial.oBaseMetalness = 1.0;
+
+const roughMetalMaterial3 = new OpenPBRMaterial();
+roughMetalMaterial3.oBaseColor = [0.4, 0.4, 0.4];
+roughMetalMaterial3.oBaseMetalness = 0.4;
+roughMetalMaterial3.oSpecularWeight = 0.2;
+
+const copperMetalMaterial = new OpenPBRMaterial();
+copperMetalMaterial.oBaseColor = [0.9, 0.6, 0.4];
+copperMetalMaterial.oBaseMetalness = 1.0;
+
+const blackPlastic = new OpenPBRMaterial();
+blackPlastic.oBaseColor = [0.25, 0.25, 0.25];
+
+const whitePlastic = new OpenPBRMaterial();
+whitePlastic.oBaseColor = [0.8, 0.8, 0.8];
+
+const greyPlastic = new OpenPBRMaterial();
+greyPlastic.oBaseColor = [0.5, 0.5, 0.5];
+greyPlastic.oSpecularWeight = 0.0;
+
+const beigeClayMaterial = new OpenPBRMaterial();
+beigeClayMaterial.oBaseColor = [0.9, 0.8, 0.7];
+beigeClayMaterial.oBaseMetalness = 0.0;
+beigeClayMaterial.oBaseDiffuseRoughness = 0.0;
+
+const MATERIAL_MAP = {
+  material_name_kunststoff_gelbFCBE37_rau_2_mtl: plasticYellow,
+  material_name_kunststoff_gelbFCBE37_rau_3_mtl: plasticYellow,
+  material_name_kunststoff_verkehrsrotB81D12_rau_5_mtl: redPlasticMaterial,
+  material_name_kunststoff_gruen9CC289_glanz_SSS_0_mtl: greenPlastic,
+  material_name_kunststoff_schwarz141414_rau_1_mtl: blackPlastic,
+  material_name_metall_stahl67686A_abgenutzt_7_mtl: metalMaterial,
+  material_name_kunststoff_weissDCDCDC_rau_6_mtl: whitePlastic,
+  // serie 45
+  material_name_kunststoff_schwarz000000_rau_0_mtl: blackPlastic,
+  material_name_kunststoff_schwarz000000_rau_31_mtl: blackPlastic,
+  material_name_metall_stahl67686A_abgenutzt_32_mtl: roughMetalMaterial,
+  material_name_kunststoff_verkehrsrotB81D12_rau_33_mtl: redPlasticMaterial,
+  material_name_kunststoff_weissDCDCDC_transparent_trueb_34_mtl: whitePlastic,
+  // serie 45 cleaned
+  material_name_kunststoff_verkehrsrotB81D12_rau_2_mtl: redPlasticMaterial,
+  material_name_kunststoff_verkehrsrotB81D12_rau_3_mtl: redPlasticMaterial,
+  material_name_kunststoff_weissDCDCDC_transparent_trueb_4_mtl: whitePlastic,
+  material_name_kunststoff_gelbFCBE37_rau_5_mtl: plasticYellow,
+  material_name_kunststoff_schwarz000000_rau_6_mtl: blackPlastic,
+  material_name_kunststoff_gruen006054_rau_7_mtl: greenPlastic,
+  material_name_metall_stahl737364_abgenutzt_rau_8_mtl: metalMaterial,
+  material_name_metall_stahl909086_abgenutzt_rauer_9_mtl: metalMaterial,
+  material_name_kunststoff_weissDCDCDC_transparent_trueb_10_mtl: whitePlastic,
+  material_name_kunststoff_schwarz000000_rau_11_mtl: blackPlastic,
+  material_name_metall_stahl67686A_abgenutzt_12_mtl: roughMetalMaterial,
+  material_name_kunststoff_verkehrsrotB81D12_rau_13_mtl: redPlasticMaterial,
+  material_name_kunststoff_weissDCDCDC_transparent_trueb_14_mtl: whitePlastic,
+  material_name_kunststoff_schwarz000000_rau_15_mtl: blackPlastic,
+  material_name_kunststoff_schwarz000000_rau_16_mtl: blackPlastic,
+  material_name_metall_stahl67686A_abgenutzt_17_mtl: roughMetalMaterial,
+  material_name_kunststoff_weissDCDCDC_transparent_trueb_18_mtl: whitePlastic,
+};
+
 async function run(target: number, yielder: any) {
-  const renderTimeElement = document.createElement("div");
-  renderTimeElement.textContent = `Finished setup`;
-  document.body.appendChild(renderTimeElement);
+  const runStartGroup = logGroup("full-model");
 
-  const TARGET_SAMPLES = 0; //199; // 200
+  const MODEL_URL =
+    target === 0
+      ? MODEL_URL_FULL
+      : target === 1
+        ? MODEL_URL_MID
+        : MODEL_URL_BARE_BONES;
 
-  console.info("start rendering", window.performance.now());
-  const buildRenderLoop = () => {
-    const renderLoopStart = logGroup("render loop full");
-    let state: "running" | "halted" = "running";
+  const model: any = await loadGltf(MODEL_URL);
+  const sceneMatrixWorld = model.scene.matrixWorld;
 
-    const isHalted = () => state === "halted";
+  model.scene.traverseVisible((object: any) => {
+    if (object.material === undefined) {
+      return;
+    }
+    const materialName = object.material.name;
+    if (materialName in MATERIAL_MAP) {
+      object.material = MATERIAL_MAP[materialName as keyof typeof MATERIAL_MAP];
+    } else {
+      console.log(materialName);
+      object.material = defaultBlueMaterial;
+    }
+  });
 
-    let currentAnimationFrameRequest: number | null = null;
-    let currentSample = 0;
-    let renderAgg = 0;
-    let renderTimes = [];
-
-    const render = async () => {};
-    currentAnimationFrameRequest = requestAnimationFrame(render);
-
-    return {
-      terminateLoop: () => {
-        state = "halted";
-        if (currentAnimationFrameRequest !== null) {
-          cancelAnimationFrame(currentAnimationFrameRequest);
-        }
-      },
-    };
-  };
-
-  buildRenderLoop();
-
-  /*controls.addEventListener("change", () => {
-    renderLoop.terminateLoop();
-
-    renderLoop = buildRenderLoop();
-  });*/
+  runPathTracer("render-target", model, {
+    targetSamples: 99,
+    kTextureWidth: 512,
+    matrixWorldContent: [
+      -0.32948748533091665, -2.7755575615628914e-17, 0.9441599424940185, 0,
+      -0.39564488300979206, 0.9079657694010268, -0.1380698668941539, 0,
+      -0.8572649086242106, -0.4190443432335072, -0.29916335812649514, 0,
+      -167.11821635612353, -83.14592338588088, -43.26162715863958, 1,
+    ],
+    fov: 23.6701655,
+    finishedSampling: (params) => {
+      const fullRunTime = runStartGroup.end();
+      yielder({
+        fullRunTime,
+        ...params,
+      });
+    },
+  });
 }
 
 function getStandardDeviation(list: number[]) {
