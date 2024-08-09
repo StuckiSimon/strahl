93a80bd04d71f47a4126cb6a49916ad5ed34a0a3
Simon Stucki <simon_stucki@hotmail.com>
2024-08-09 09:42:05 +0200

    Improve exception message

--- strahl-lib/src/exceptions.ts
+++ strahl-lib/src/exceptions.ts
@@ -1,7 +1,7 @@
 export class WebGPUNotSupportedError extends Error {
   constructor(techReason: string) {
     super(
-      `WebGPU is not supported in this environment. Make sure to use a browser and device that supports WebGPU. (Reason: ${techReason})`,
+      `[WebGPUNotSupportedError] WebGPU is not supported in this environment. Make sure to use a browser and device that supports WebGPU. (Reason: ${techReason})`,
     );
   }
 }
@@ -9,7 +9,7 @@ export class WebGPUNotSupportedError extends Error {
 export class CanvasReferenceError extends Error {
   constructor() {
     super(
-      "Canvas reference is not set. Make sure to set the canvas to a DOM node before calling any methods.",
+      "[CanvasReferenceError] Canvas reference is not set. Make sure to set the canvas to a DOM node before calling any methods.",
     );
   }
 }
@@ -17,7 +17,7 @@ export class CanvasReferenceError extends Error {
 export class SignalAlreadyAbortedError extends Error {
   constructor() {
     super(
-      "Signal is already aborted. Make sure to check the aborted property before calling any methods.",
+      "[SignalAlreadyAbortedError] Signal is already aborted. Make sure to check the aborted property before calling any methods.",
     );
   }
 }
@@ -25,7 +25,7 @@ export class SignalAlreadyAbortedError extends Error {
 export class InvalidMaterialError extends Error {
   constructor(public readonly invalidMaterial: unknown) {
     super(
-      "Material is invalid. Make sure to configure all materials to be instances of OpenPBRMaterial.",
+      "[InvalidMaterialError] Material is invalid. Make sure to configure all materials to be instances of OpenPBRMaterial.",
     );
   }
 }
@@ -33,7 +33,7 @@ export class InvalidMaterialError extends Error {
 export class InternalError extends Error {
   constructor(techReason: string) {
     super(
-      `Library error occurred, please get in contact. (Reason: ${techReason})`,
+      `[InternalError] Library error occurred, please get in contact. (Reason: ${techReason})`,
     );
   }
 }
