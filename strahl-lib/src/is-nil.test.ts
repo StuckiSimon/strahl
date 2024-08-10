7aeb67ddf92da6e87c1a06f486e3064b00940d02
Simon Stucki <simon_stucki@hotmail.com>
2024-08-10 07:25:04 +0200

    Write is-nil test

---
+++ strahl-lib/src/is-nil.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from "vitest";
+import { isNil } from "./is-nil";
+
+describe("isNil", () => {
+  it("should return true for null", () => {
+    expect(isNil(null)).toBe(true);
+  });
+
+  it("should return true for undefined", () => {
+    expect(isNil(undefined)).toBe(true);
+  });
+
+  it("should return false for a value", () => {
+    expect(isNil(1)).toBe(false);
+  });
+});
