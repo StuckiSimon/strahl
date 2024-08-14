9c6efb917f2a5d99260c66332de6db74d1578bd2
Simon Stucki <simon_stucki@hotmail.com>
2024-08-14 07:23:08 +0200

    Update library entrypoint file
    Add basic structure

--- strahl-lib/src/index.ts
+++ strahl-lib/src/index.ts
@@ -1,5 +1,17 @@
+/**
+ * Core Exports
+ */
 export { OpenPBRMaterial } from "./openpbr-material";
 import runPathTracer from "./path-tracer";
+export { runPathTracer };
+
+/**
+ * Util Exports
+ */
+export { convertHexToRGB } from "./util/hex-to-rgb";
+
+/**
+ * Type Exports
+ */
 export type { PathTracerOptions } from "./path-tracer";
 export type { EnvironmentLightConfig } from "./environment-light";
-export { runPathTracer };
