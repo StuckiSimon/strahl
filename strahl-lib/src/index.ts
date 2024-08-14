7a0fb8a4f61d4c3936da48ab80f22f27b4947d7d
Simon Stucki <simon_stucki@hotmail.com>
2024-08-14 14:56:12 +0200

    Export base types

--- strahl-lib/src/index.ts
+++ strahl-lib/src/index.ts
@@ -13,5 +13,6 @@ export { convertHexToRGB } from "./util/hex-to-rgb";
 /**
  * Type Exports
  */
+export type { Color, Vec3 } from "./core/types";
 export type { PathTracerOptions } from "./path-tracer";
 export type { EnvironmentLightConfig } from "./environment-light";
