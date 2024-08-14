46d7a1b6d43a39ff163b7602cbf980d24fb67db3
Simon Stucki <simon_stucki@hotmail.com>
2024-08-14 07:50:04 +0200

    Document hex-to-rgb helper

--- strahl-lib/src/util/hex-to-rgb.ts
+++ strahl-lib/src/util/hex-to-rgb.ts
@@ -8,10 +8,15 @@ function convertEightBitToNormalized(value: number): number {
   return value / 255;
 }
 
+/**
+ * Convert a hex color string to a normalized color array for use within strahl.
+ * @param hex The hex color string (e.g. #ff0055)
+ * @returns The color in normalized format (0-1)
+ */
 export function convertHexToRGB(hex: string): Color {
   const o = hex[0] === "#" ? 1 : 0;
   return [
-    convertEightBitToNormalized(parseHex(hex.slice(o , o + 2))),
+    convertEightBitToNormalized(parseHex(hex.slice(o, o + 2))),
     convertEightBitToNormalized(parseHex(hex.slice(o + 2, o + 4))),
     convertEightBitToNormalized(parseHex(hex.slice(o + 4, o + 6))),
   ];
